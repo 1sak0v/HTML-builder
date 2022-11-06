e2e9608cf3a5b85ffad0a5d8cfb00b96002b8b61
Anton Vyun <zeta664433@gmail.com>
2022-11-06 23:02:01 +0300

    feat: solve copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const path = require('path');
+const fs = require('fs/promises');
 
+const copyDir = async () => {
+    try {
+        await fs.rm(path.resolve(__dirname, 'files-copy'), {recursive: true, force: true});
+        await fs.mkdir(path.resolve(__dirname, 'files-copy'), {recursive: true});
+
+        const files = await fs.readdir(path.resolve(__dirname, 'files'));
+
+        for(const file of files) {
+            const fromPath = path.resolve(__dirname, 'files', file);
+            const toPath = path.resolve(__dirname, 'files-copy', file);
+            await fs.copyFile(fromPath, toPath);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+copyDir();
